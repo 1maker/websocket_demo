77d4a585add11cefcae60308d72c06d2c681df05
Shayakhmetov Airat <4321494+1maker@users.noreply.github.com>
2020-05-28 20:25:49 +0500

    Я понял как устроены конструкторы в JavaScript

--- new_feature.js
+++ new_feature.js
@@ -1,9 +1,9 @@
 function How_to_make_the_billion_of_Russion_rubles(){
 
-	console.log('Я не знаю');
+	
 }
 
 function (){
 
-  new How_to_make_the_billion_of_Russion_rubles()
+  let obj = new How_to_make_the_billion_of_Russion_rubles()
 }
