e3cc1c0662fb29e33ba90789342e2290489409de
Shayakhmetov Airat <1maker0n@gmail.com>
2020-06-09 18:40:14 +0500

    Вебсокет-сервер работает

--- new_feature.js
+++
@@ -1,9 +0,0 @@
-function How_to_make_the_billion_of_Russion_rubles(){
-
-	
-}
-
-function (){
-
-  let obj = new How_to_make_the_billion_of_Russion_rubles()
-}
--- web_socket_server.js
+++ web_socket_server.js
@@ -1,37 +1,100 @@
+const uuid = require('uuid');
+const session = require('express-session');
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+const server = require('http').createServer(app);
+
+/*const sessionParser = session({
+	saveUninitialized: false,
+	secret: '$eCuRiTy',
+	resave: false
+});*/
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ 
+									clientTracking: true, 
+									noServer: true 
+								});
+
+const clients = new Set();
+
+server.on('upgrade', function(request, socket, head){
 	
-	res.sendFile(__dirname + '\\public\\index.html');
+	console.log('Parsing session from request...');
+
+	wss.handleUpgrade(request, socket, head, function(ws){
+	
+		clients.add(ws);
+	
+		ws.on('message', (message) => {
+			
+			//console.log(request);
+			//console.log(socket);
+			
+			for (let client of clients){
+				
+				client.send(message + '***');
+			}
+		
+			/*wss.clients.forEach(function each(client){
+				
+				if (client.readyState === WebSocket.OPEN){
+					client.send(data);
+				}
+			});*/
+		});
+
+		ws.on('close', function(){
+			
+			clients.delete(ws);
+			
+			console.log('client is closed');
+		});
+	});
 });
 
-app.get('/get_badges', async (req, res) => {
+server.listen(9000, () => {
+	
+	console.log('listening on *:9000');
+});
+
+/*app.get('/get_websocket', async (req, res) => {
 	
 	res.set({
 		'Access-Control-Allow-Methods':'GET,POST,PATCH',
 		'Access-Control-Allow-Headers':'the_key_to_the_apartment_where_the_money_is,Content-Type',
 	});
 	
-	const connection = await require('./scripts/connection_to_db.js');
-	
-	let [cars_models_count] = await connection.execute('SELECT COUNT(*) as cars_models_count FROM cars_models');
-	let [cars_models_available_count] = await connection.execute('SELECT COUNT(*) as cars_models_available_count FROM cars_models_available');
-	
-	//connection.end();
-	
-	cars_models_count = cars_models_count[0].cars_models_count;
-	cars_models_available_count = cars_models_available_count[0].cars_models_available_count;
-	
-	res.send(JSON.stringify({cars_models_count, cars_models_available_count}));
-});
+	res.send(JSON.stringify({"test":"9876543210"}));
+});*/
 
-let listen_id = http.listen(9000, () => {
-  console.log('listening on *:9000');
-});
+//app.use(sessionParser);
+
+/*app.get('/start', (req, res) => {
+	
+	//console.log('Идейно, это должно отработать');
+	//res.sendFile(__dirname + '\\index.html');
+	//res.send('<h1>Всё работает, всё нормально!</h1>');
+});*/
+
+/*sessionParser(request, {}, () => {
+		
+	if (!request.session.userId){
+		
+		socket.destroy();
+		return;
+	}
+
+	console.log('Session is parsed!');
+
+	wss.handleUpgrade(request, socket, head, function(ws) {
+		
+		wss.emit('connection', ws, request);
+	});
+});*/
+
 
 
 
