92c3012326ae0cbd5a80f7f15ec3fdf747972e70
Shayakhmetov Airat <1maker0n@gmail.com>
2020-06-11 14:49:09 +0500

    Начал рисовать картины на канвасе. Предполагаю ввести на одном холсте разные цветовые кисти

--- web_socket_server.js
+++ web_socket_server.js
@@ -1,54 +1,67 @@
-const uuid = require('uuid');
 const session = require('express-session');
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
+const http = require('http')
+const server = http.createServer(app);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ clientTracking: true, noServer: true });
+const colors = [
+	{'black':'#000000'}, 
+	{'strong_blue':'#0099cc'},
+	{'dark_green':'#339900'}, 
+	{'dark_pink':'#990033'}, 
+	{'pure_yellow':'#ccff00'}
+];
 
-/*const sessionParser = session({
-	saveUninitialized: false,
-	secret: '$eCuRiTy',
-	resave: false
-});*/
+let matching = {};	// объект сопоставления цветов и подключенных клиентов
+
+function random_key_in_colors_array(min, max) {
+  
+	// случайное число от min до (max+1)
+	
+	return Math.floor(min + Math.random() * (max + 1 - min));
+}
 
 app.use(express.static(__dirname));
 
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ 
-									clientTracking: true, 
-									noServer: true 
-								});
-
-const clients = new Set();
-
-server.on('upgrade', function(request, socket, head){
+server.on('upgrade', function(request, socket, head) {
 	
-	console.log('Parsing session from request...');
-
-	wss.handleUpgrade(request, socket, head, function(ws){
-	
-		clients.add(ws);
-	
-		ws.on('message', (message) => {
-			
-			//console.log(request);
-			//console.log(socket);
-			
-			for (let client of clients){
-				
-				client.send(message + '***');
-			}
+	wss.handleUpgrade(request, socket, head, (ws) => {
 		
-			/*wss.clients.forEach(function each(client){
-				
-				if (client.readyState === WebSocket.OPEN){
-					client.send(data);
-				}
-			});*/
-		});
-
-		ws.on('close', function(){
+		let random_key = random_key_in_colors_array(0, colors.length - 1);
+		
+		while (Object.values(matching).find(item => item === Object.values(colors[random_key])[0])) {
+		
+			random_key = random_key_in_colors_array(0, colors.length - 1);
+		}
+		
+		matching[request.headers['sec-websocket-key']] = Object.values(colors[random_key])[0];
+		
+		ws['colors_colors'] = Object.values(colors[random_key])[0];
+		
+		console.log(matching);
+		
+		console.log('Количество подключенных клиентов', wss.clients.size);
+		
+		//console.log(ws);
+		
+		ws.on('message', (coordinates) => {
+		
+			const {x, y} = JSON.parse(coordinates);
 			
-			clients.delete(ws);
+			for (let client of wss.clients){
+				
+				//console.log(matching.find(item => item === client['colors_colors']));
+				//console.log(JSON.stringify({x, y, color:client['colors_colors']}));
+					
+				client.send(JSON.stringify({x, y, color:client['colors_colors']}));
+			}
+		});
+		
+		ws.on('close', () => {
+			
+			wss.clients.delete(ws);
+			delete matching[request.headers['sec-websocket-key']];
 			
 			console.log('client is closed');
 		});
@@ -59,42 +72,3 @@ server.listen(9000, () => {
 	
 	console.log('listening on *:9000');
 });
-
-/*app.get('/get_websocket', async (req, res) => {
-	
-	res.set({
-		'Access-Control-Allow-Methods':'GET,POST,PATCH',
-		'Access-Control-Allow-Headers':'the_key_to_the_apartment_where_the_money_is,Content-Type',
-	});
-	
-	res.send(JSON.stringify({"test":"9876543210"}));
-});*/
-
-//app.use(sessionParser);
-
-/*app.get('/start', (req, res) => {
-	
-	//console.log('Идейно, это должно отработать');
-	//res.sendFile(__dirname + '\\index.html');
-	//res.send('<h1>Всё работает, всё нормально!</h1>');
-});*/
-
-/*sessionParser(request, {}, () => {
-		
-	if (!request.session.userId){
-		
-		socket.destroy();
-		return;
-	}
-
-	console.log('Session is parsed!');
-
-	wss.handleUpgrade(request, socket, head, function(ws) {
-		
-		wss.emit('connection', ws, request);
-	});
-});*/
-
-
-
-
